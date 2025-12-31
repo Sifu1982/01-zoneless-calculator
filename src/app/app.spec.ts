9d4251decdb607e48c9e5d8326c7fb97e61ded54
dmartinezt <dmartinezt.pex@sanitas.es>
2025-12-31 13:13:13 +0100

    app tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -2,22 +2,79 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
-  });
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     imports: [App],
+  //   }).compileComponents();
+  // });
+
+  // it('should be 4', () => {
+  //   // Arrange
+
+  //   const num1 = 1;
+  //   const num2 = 3;
+
+  //   // Act
+
+  //   const result = num1 + num2;
+
+  //   //Assert
+
+  //   expect(result).toBe(4);
+  // });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    // console.log(compiled.innerHTML);
+
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render router-outlet', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+
+    const routerOutlet = compiled.querySelector('router-outlet');
+
+    expect(routerOutlet).toBeTruthy();
+  });
+
+  it('should render router-outlet with css classes', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const divElement = compiled.querySelector('div');
+    const mostHaveClasses =
+      'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(
+        ' '
+      );
+
+    // expect(divElement?.classList.value).toBe(mostHaveClasses);
+    divElement?.classList.forEach((className) => {
+      expect(mostHaveClasses).toContain(className);
+    });
+  });
+
+  it('should render buy me a beer link', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const linkElement = compiled.querySelector('a');
+    expect(linkElement).toBeTruthy();
+
+    const titleAttribute = linkElement?.getAttribute('title');
+    const hrefAttribute = linkElement?.getAttribute('href');
+    const targetAttribute = linkElement?.getAttribute('target');
+
+    const expectedTitle = 'Buy me a beer';
+    const expectedHref = 'https://www.buymeacoffee.com/scottwindon';
+    const expectedTarget = '_blank';
+
+    expect(titleAttribute).toBe(expectedTitle);
+    expect(hrefAttribute).toBe(expectedHref);
+    expect(targetAttribute).toBe(expectedTarget);
   });
 });
